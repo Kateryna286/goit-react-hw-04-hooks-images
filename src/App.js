8c952e73928136f4f6280c692ce52cefb263a148
Kateryna Karpova <katyakarpova@gmail.com>
2021-09-22 15:02:45 +0200

    Refactoring App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Searchbar from './components/Searchbar/Searchbar';
 import ImageGallery from './components/ImageGallery/ImageGallery';
 import './App.css';
 
-export default class App extends Component {
-  state = {
-    keyWord: '',
+export default function App() {
+  const [keyWord, setKeyWord] = useState('');
+
+  const formSubmitHandler = data => {
+    setKeyWord(data);
   };
 
-  formSubmitHandler = data => {
-    this.setState({ keyWord: data });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.formSubmitHandler} />
-        <ImageGallery keyWord={this.state.keyWord} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Searchbar onSubmit={formSubmitHandler} />
+      <ImageGallery keyWord={keyWord} />
+    </div>
+  );
 }
 
 //my key 22564694-3177f5daba1f2572eee652a36;
