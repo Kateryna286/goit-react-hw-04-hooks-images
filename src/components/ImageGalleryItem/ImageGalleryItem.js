d2e7d0bcdd7911659c0475f7d4e297c6659712eb
Kateryna Karpova <katyakarpova@gmail.com>
2021-09-22 15:10:02 +0200

    Refactoring ImageGalleryItem.js

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,20 +1,14 @@
-import React, { Component } from 'react';
 import './ImageGalleryItem.css';
 
-class ImageGalleryItem extends Component {
-  getId = event => {
+export default function ImageGalleryItem({ onClick, id, src, alt }) {
+  const getId = event => {
     const id = +event.currentTarget.id;
-    this.props.onClick(id);
+    onClick(id);
   };
 
-  render() {
-    const { id, src, alt } = this.props;
-    return (
-      <li id={id} className="ImageGalleryItem" onClick={this.getId}>
-        <img src={src} alt={alt} className="ImageGalleryItem-image" />
-      </li>
-    );
-  }
+  return (
+    <li id={id} className="ImageGalleryItem" onClick={getId}>
+      <img src={src} alt={alt} className="ImageGalleryItem-image" />
+    </li>
+  );
 }
-
-export default ImageGalleryItem;
