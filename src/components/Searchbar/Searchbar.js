71023b0a1b5f53ac518e1d7242a48b442d660bb2
Kateryna Karpova <katyakarpova@gmail.com>
2021-09-22 21:53:26 +0200

    Refactoring Searchbar.js

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Searchbar.css';
 
-export class Searchbar extends Component {
-  state = {
-    keyWord: '',
+export default function Searchbar({ onSubmit }) {
+  const [keyWord, setKeyWord] = useState('');
+
+  const handleChange = event => {
+    setKeyWord(event.target.value);
   };
 
-  handleChange = event => {
-    const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-
-    const { keyWord } = this.state;
-    const { onSubmit } = this.props;
 
     if (keyWord.trim() === '') {
       alert('Введите ключевое слово');
@@ -23,39 +17,33 @@ export class Searchbar extends Component {
     }
 
     onSubmit(keyWord);
-    this.resetForm();
+    resetForm();
   };
 
-  resetForm = () => {
-    this.setState({
-      keyWord: '',
-    });
+  const resetForm = () => {
+    setKeyWord('');
   };
 
-  render() {
-    return (
-      <>
-        <header className="Searchbar">
-          <form onSubmit={this.handleSubmit} className="SearchForm">
-            <button type="submit" className="SearchForm-button">
-              <span className="SearchForm-button-label">Search</span>
-            </button>
+  return (
+    <>
+      <header className="Searchbar">
+        <form onSubmit={handleSubmit} className="SearchForm">
+          <button type="submit" className="SearchForm-button">
+            <span className="SearchForm-button-label">Search</span>
+          </button>
 
-            <input
-              className="SearchForm-input"
-              type="text"
-              autoComplete="off"
-              autoFocus
-              placeholder="Search images and photos"
-              name="keyWord"
-              value={this.state.keyWord}
-              onChange={this.handleChange}
-            />
-          </form>
-        </header>
-      </>
-    );
-  }
+          <input
+            className="SearchForm-input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            name="keyWord"
+            value={keyWord}
+            onChange={handleChange}
+          />
+        </form>
+      </header>
+    </>
+  );
 }
-
-export default Searchbar;
